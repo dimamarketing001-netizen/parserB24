c45913f61c69378dc92bda7f8345a9d4cd105596
dimamarketing001-netizen <dimamarketing001@gmail.com>
2026-05-20 16:32:07 +0500

    re

--- parserWebhook.py
+++ parserWebhook.py
@@ -26,7 +26,7 @@ logging.basicConfig(
 )
 
 # Укажите дату, с которой начинать выгрузку лидов, в формате ГГГГ-ММ-ДД
-START_DATE = "2025-01-01"
+START_DATE = "2026-05-19"
 
 # Список вебхуков Bitrix24 (из переменных окружения)
 _raw_webhooks = os.getenv('BITRIX_WEBHOOKS', '')
